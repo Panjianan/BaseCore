53dfb11037a04d18a97e6248a4fbdf68d80b494a
TsubasaP91 <258687621@qq.com>
2017-11-13 18:13:51 +0800

    抽离gralde依赖管理

--- core/src/main/java/com/tsubasa/core/model/exception/APIException.kt
+++ core/src/main/java/com/tsubasa/core/model/exception/APIException.kt
@@ -1,8 +1,7 @@
 package com.tsubasa.core.model.exception
 
 /**
+ * 代表请求错误的
  * Created by tsubasa on 2017/11/12.
  */
-class APIException : RuntimeException() {
-
-}
+class APIException(val code: Int, val errorMsg: CharSequence) : RuntimeException(errorMsg.toString())
--- core/src/main/java/com/tsubasa/core/model/exception/DataException.kt
+++ core/src/main/java/com/tsubasa/core/model/exception/DataException.kt
@@ -1,8 +1,7 @@
 package com.tsubasa.core.model.exception
 
 /**
+ * 和数据相关的异常
  * Created by tsubasa on 2017/11/12.
  */
-class DataException : RuntimeException() {
-
-}
+class DataException(errorMsg: CharSequence? = "") : RuntimeException(errorMsg?.toString() ?: "")
